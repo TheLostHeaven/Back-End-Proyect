a07c571a3a431adde1c00b200668584202fa0dde
Daniel <Dannyking07@outlook.es>
2024-09-13 14:54:04 -0500

    feat(app): add config from routers

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,15 @@
 import express from 'express'
 import cors from 'cors'
-
-//import { router } from './routers'; 
-//import { routes } from './routers/service';
-
-
+import { router } from './routers';
 
 const app = express();
+
 
 //AppUse
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(router)
+
 
 app.use(
     cors({
