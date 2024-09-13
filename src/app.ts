fc320b482033db8a830216b7c189c25e9b69fc4e
Daniel <Dannyking07@outlook.es>
2024-09-13 14:14:16 -0500

    config initial setup

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express from 'express'
+import cors from 'cors'
+
+//import { router } from './routers'; 
+//import { routes } from './routers/service';
+
+
+
+const app = express();
+
+//AppUse
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(
+    cors({
+        origin: "http://localhost:4200",
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        credentials: true,
+        preflightContinue: false,
+        optionsSuccessStatus: 204,
+    })
+);
+
+export default app;
+
+
